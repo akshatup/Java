43b988b966a16240ea45a2094f6c47cc050fe8e3
akshatup <akshatupasani13@gmailcom>
2025-09-01 01:47:00 +0530

    arithmetic_operators

---
+++ Operators/arithmetic_operators1.java
@@ -0,0 +1,35 @@
+//  import java.util.*;
+
+// Binary
+
+// Assignment                :- =
+// Addition                  :- +
+// Subtraction               :- -
+// Multiplication            :- *
+// Division                  :- /
+// Modulus (remainder)       :- %
+
+public class arithmetic_operators1{
+    public static void main (String args[]){
+
+        int a = 21;
+        int b = 3;
+
+        System.out.println("1st no.:- "+ a + " \n2nd no.:- " + b );
+        
+
+        System.out.println("Addition of two nos:- " + (a+b));  //  add
+
+        System.out.println("Subtraction of two nos:- "+ (a-b)); // subtract
+
+        System.out.println("Multiplication of two nos:- "+ (a*b)); // multiply
+
+        System.out.println("Division of two nos:- " + (a/b)); // Division
+
+        System.out.println("Modulus of two nos:- " + (a%b));  // Modulus
+
+
+
+    
+    }
+}
