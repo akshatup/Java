91e6ae833e81af53fcdfb87292be5b9b78cd1ff0
akshatup <akshatupasani13@gmailcom>
2025-09-01 01:48:55 +0530

    Relational_operators

---
+++ Operators/Relational_operators.java
@@ -0,0 +1,40 @@
+public class Relational_operators {
+
+    // Equal to                  :- ==
+    // Not equal to (inequality) :- !=
+    // Greater than              :- >
+    // Less than                 :- <
+    // Greater than or equal to  :- >=
+    // Less than or equal to     :- <=
+
+    public static void main(String args[]){
+
+        // int a = 10;
+        // int b = 5;
+
+        // System.out.println("int a = "+ a);
+        // System.out.println("int b = "+ b);
+
+        // System.out.println("Result of a==b operator :- " + (a==b));   // Equal to
+        // System.out.println("Result of a!=b operator:- " + (a!=b));   // Not equal to
+        // System.out.println("Result of a<b operator:- " + (a<b));    // Less than
+        // System.out.println("Result of a>b operator:- " + (a>b));    // Greater than
+        // System.out.println("Result of a<=b operator:- " + (a<=b));   // Less than or equal to
+        // System.out.println("Result of a>=b operator:- " + (a>=b));   // Greater than or equal to
+
+        int a = 10;
+        int b = 10;
+
+        System.out.println("int a = "+ a);
+        System.out.println("int b = "+ b);
+
+        System.out.println("Result of a==b operator:- " + (a==b));
+        System.out.println("Result of a!=b opeartor:- " + (a!=b));
+        System.out.println("Result of a<b opeartor:- " + (a<b));
+        System.out.println("Result of a>b opeartor:- " + (a>b));
+        System.out.println("Result of a<=b opeartor:- " + (a<=b));
+        System.out.println("Result of a>=b opeartor:- " + (a>=b));
+        
+    }
+    
+}
