ab0b5946ec1bc5460ccde77ca66ff698cfcbce91
akshatup <akshatupasani13@gmailcom>
2025-09-20 21:01:45 +0530

    while_loop

---
+++ Loops/while_loop.java
@@ -0,0 +1,19 @@
+package Loops;
+
+
+public class while_loop {
+
+    public static void main(String args[] ) {
+        
+        int count = 0;
+
+        while(count<10){
+
+            System.out.println("hi");
+            count++;
+        }
+
+        System.out.println("printed \"hi\" 10 times");
+    }
+    
+}
