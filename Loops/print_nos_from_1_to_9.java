0f433e0da131dc75b6f15fcb94868743e4720115
akshatup <akshatupasani13@gmailcom>
2025-09-21 19:39:25 +0530

    print_numbers_from_1_to_9

---
+++ Loops/print_nos_from_1_to_9.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class print_nos_from_1_to_9 {
+
+    public static void main(String args[] ) {
+
+        int number = 1;
+
+        while(number<10){
+
+            // System.out.print(number);
+            // System.out.println(number);
+            System.out.print(number + " ");
+            number++;
+        }
+
+
+        
+    }
+    
+}
