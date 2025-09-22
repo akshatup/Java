82e6164911b8205714ea65041c44542872e55e22
akshatup <akshatupasani13@gmailcom>
2025-09-22 18:38:41 +0530

    print_square_pattern

---
+++ Loops/print_square_pattern.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class print_square_pattern {
+
+    public static void main(String args[] ) {
+
+        for(int line=1; line<=4; line++){
+
+            System.out.println("****");
+
+
+        }
+
+        
+        
+    }
+    
+}
