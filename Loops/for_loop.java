eb98917d7e3bf74ca69da14451558f3813f52508
akshatup <akshatupasani13@gmailcom>
2025-09-22 18:38:20 +0530

    for_loop

---
+++ Loops/for_loop.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class for_loop {
+
+    public static void main(String args [] ) {
+
+        for( int i=1; i<=10; i++){
+            System.out.println("hello world");
+        }
+
+        System.out.println("printed \"hello world\" 10 times");
+        
+    }
+    
+}
